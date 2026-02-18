2e4d127644b662fc84b536b81f88c90c51c36cca
Suji V <Suji26262@gmail.com>
2026-02-18 09:55:21 +0530

    Pwa features add

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      includeAssets: ['icon-192.png', 'icon-512.png'],
+      manifest: {
+        name: 'My App',
+        short_name: 'MyApp',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#121212',
+        theme_color: '#4caf50',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
